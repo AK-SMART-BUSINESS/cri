ee50fa72d2fe7f77c14672920335fd63baec2fe2
git-ci <56820280+git-ci@users.noreply.github.com>
2019-11-27 17:42:21 +0000

    Ajout du module ckeditor et reconfiguration du modèle

--- cri/settings.py
+++ cri/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor_uploader',
+    'ckeditor',
 ]
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mainsite/models.py
+++ mainsite/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from ckeditor_uploader.fields import RichTextUploadingField
+
 # Create your models here.
 class Home(models.Model):
     banner = models.ImageField(upload_to='uploads/')
-    content = models.TextField()
+    content = RichTextUploadingField()
     publish = models.BooleanField()
 
 class About(models.Model):
     banner = models.ImageField(upload_to='uploads/')
-    content = models.TextField()
+    content = RichTextUploadingField()
     publish = models.BooleanField()
