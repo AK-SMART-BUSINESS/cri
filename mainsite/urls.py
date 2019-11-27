15e1b9ab6fa98f68f03dfdf5166d0d3e0258fe7c
git-ci <56820280+git-ci@users.noreply.github.com>
2019-11-27 23:47:29 +0000

    Mise en place du font-office

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -5,5 +5,18 @@ from . import views
 app_name = 'mainsite'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('apropos', views.about, name='about')
+    path('departements', views.departements, name="departements"),
+    path('departement/recherche', views.recherche, name="recherche"),
+    path('departement/formation', views.formation, name="formation"),
+    path('departement/ingenierie', views.ingenierie, name="ingenierie"),
+    path('departement/communication', views.communication, name="communication"),
+    path('projets', views.projets, name="projets"),
+    path('expertises', views.expertises, name="expertises"),
+    path('partenariats', views.partenariats, name="partenariats"),
+    path('contacts', views.contacts, name="contacts"),
+    path('mot-du-directeur', views.manager, name="manager"),
+    path('vision-but-objectifs', views.vision_but_objectifs, name="vision_but_objectifs"),
+    path('mission', views.mission, name="mission"),
+    path('nos-valeurs', views.nos_valeurs, name="nos_valeurs")
+    # path('apropos', views.about, name='about')
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -10,5 +10,41 @@ def index(request):
     }
     return render(request, 'mainsite/index.html', context)
 
-def about(request):
-    return render(request, 'mainsite/aboutus.html')
+def departements(request):
+    return render(request, 'mainsite/departement.html')
+
+def projets(request):
+    return render(request, 'mainsite/projet.html')
+
+def expertises(request):
+    return render(request, 'mainsite/expertise.html')
+
+def partenariats(request):
+    return render(request, 'mainsite/partenaire.html')
+
+def contacts(request):
+    return render(request, 'mainsite/contact.html')
+
+def recherche(request):
+    return render(request, 'mainsite/recherche.html')
+
+def formation(request):
+    return render(request, 'mainsite/formation.html')
+
+def ingenierie(request):
+    return render(request, 'mainsite/ingenierie.html')
+
+def communication(request):
+    return render(request, 'mainsite/communication.html')
+
+def manager(request):
+    return render(request, 'mainsite/manager.html')
+
+def vision_but_objectifs(request):
+    return render(request, 'mainsite/vbo.html')
+
+def mission(request):
+    return render(request, 'mainsite/mission.html')
+
+def nos_valeurs(request):
+    return render(request, 'mainsite/valeurs.html')
